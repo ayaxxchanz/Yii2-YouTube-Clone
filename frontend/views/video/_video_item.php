70d2e55bc91a0004b65ae050663f237081bce6d2
Aliya <aliya.genius@gmail.com>
2022-08-02 12:02:54 +0800

    Feat: Insert link to video view page

--- frontend/views/video/_video_item.php
+++ frontend/views/video/_video_item.php
@@ -1,12 +1,16 @@
 <?php
 
+use yii\helpers\Url;
+
 /** @var $model \common\models\Video */
 
 ?>
 
 <div class="card m-3" style="width: 14rem;">
-    <video class="embed-responsive" poster="<?php echo $model->getThumbnailLink() ?>"
-        src="<?php echo $model->getVideoLink() ?>"></video>
+    <a href="<?php echo Url::to(['/video/view', 'id' => $model->video_id]) ?>">
+        <video class="embed-responsive" poster="<?php echo $model->getThumbnailLink() ?>"
+            src="<?php echo $model->getVideoLink() ?>"></video>
+    </a>
     <div class="card-body p-2">
         <h6 class="card-title m-0"><?php echo $model->title ?></h6>
         <p class="text-muted card-text m-0">
